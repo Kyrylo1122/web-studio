71163338515f28d7f6b7b3a80a6e91d9d212ff6d
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-11-07 16:47:22 +0100

    add css

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import AppBar from "../AppBar/Header";
-
+import Footer from "../Footer/Footer";
 export default function Layout() {
   return (
     <>
       <AppBar />
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
